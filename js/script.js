ceec1fdb94f1686c98738f329b371b878e597729
Alessandro Montenero <alemonte99@icloud.com>
2023-02-09 15:14:41 +0100

    added object

--- js/script.js
+++ js/script.js
@@ -6,8 +6,10 @@ createApp({
   return {
     message: 'Hello Vue!',
     messageColor: 'rgb(55, 174, 121)',
-    imgClass: 'logoImg',
-    imgPath: './img/Vue.js_Logo_2.svg.png'
+    objectOfImgAttrs: {
+        class: 'logoImg',
+        src: './img/Vue.js_Logo_2.svg.png'
+    }
   }
 }
 }).mount('#app')
